ae1b05b0a231fcc0ace1a64857246b73f0be0376
adubbya <aaron.mitchell@austin.utexas.edu>
2018-02-27 22:48:04 -0600

    added submit to survey, some pseudocoding

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,16 +4,21 @@ var friendsData = require("../data/friends")
 // routing
 module.exports = function (app) {
 
-    // API GET Requests
-    // GET friends
+// API Requests
+    // get friends
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
         console.log(friendsData);
     });
 }
+ /*   // post friends
+    app.post("/api/friends", function (req, res) {
+        // body-parser
+        friendsData.push(req.body);
+        res.json(true);
+        // comparison function
+    });
 
-// get 
-
-// post
-
+    // might add post clear for testing
+*/
 
